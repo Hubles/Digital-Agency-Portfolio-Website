8b4a161f46bf51037bbef21d3601215400a04157
Maga <tsaroev17@gmail.com>
2024-11-21 16:40:56 +0300

    Bug fixes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const navBar = document.querySelector('.header-nav')
 const headerBurger = document.querySelector('.header-burger')
 const navBarKrest = document.querySelector('.nav-krest')
+const body = document.body
 
 headerBurger.addEventListener('click', () => {
   navBar.classList.add('active')
+  body.style.overflowY = 'hidden'
 })
 
 navBarKrest.addEventListener('click', () => {
   navBar.classList.remove('active')
+  body.style.overflowY = ''
 })
