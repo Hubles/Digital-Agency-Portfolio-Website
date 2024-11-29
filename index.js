4accd09ac33ebe7b94c2727e208fabca2cb21ada
Maga <tsaroev17@gmail.com>
2024-11-29 21:03:53 +0300

    Services section with adaptive

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const navBar = document.querySelector('.header-nav')
-const headerBurger = document.querySelector('.header-burger')
-const navBarKrest = document.querySelector('.nav-krest')
-const body = document.body
+const navBar = document.querySelector(".header-nav");
+const headerBurger = document.querySelector(".header-burger");
+const navBarKrest = document.querySelector(".nav-krest");
+const body = document.body;
 
-headerBurger.addEventListener('click', () => {
-  navBar.classList.add('active')
-  body.style.overflowY = 'hidden'
-})
+headerBurger.addEventListener("click", () => {
+  navBar.classList.add("active");
+  body.style.overflowY = "hidden";
+});
 
-navBarKrest.addEventListener('click', () => {
-  navBar.classList.remove('active')
-  body.style.overflowY = ''
-})
+navBarKrest.addEventListener("click", () => {
+  navBar.classList.remove("active");
+  body.style.overflowY = "";
+});
 
-function summ(a, b) {
-  return a + b
+// Read and Less buttons
+
+const servicesItemText = document.querySelectorAll(".serv-disc");
+
+if (window.innerWidth <= 1024) {
+
+  for (let text of servicesItemText) {
+    text.innerHTML = `${text.innerHTML.slice(0,126)}<span class="readClick"> Read more...</span>`;
+  }
+
 }
-
-console.log(summ(2, 4))
